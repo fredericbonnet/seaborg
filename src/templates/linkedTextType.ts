78ae6f48d22a101c149ed3a21b049f88820da1b9
Frédéric Bonnet <fredericbonnet@free.fr>
2019-09-28 14:57:54 +0200

    Add linkedTextType and refTextType

---
+++ src/templates/linkedTextType.ts
@@ -0,0 +1,20 @@
+/*
+  <xsd:complexType name="linkedTextType" mixed="true">
+    <xsd:sequence>
+    <xsd:element name="ref" type="refTextType" minOccurs="0" maxOccurs="unbounded" />
+    </xsd:sequence>
+  </xsd:complexType>
+*/
+
+import { Element } from '@rgrove/parse-xml';
+import { Mappers, applyToChildren, $text } from '.';
+
+import refTextType from './refTextType';
+import textNode from './textNode';
+
+const mappers: Mappers = {
+  ref: refTextType,
+  [$text]: textNode,
+};
+
+export default (element: Element) => applyToChildren(mappers)(element).join('');
