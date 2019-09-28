17ce325b43ed4b0426c10cd7a9d99ce074140aaa
Frédéric Bonnet <fredericbonnet@free.fr>
2019-09-28 22:26:23 +0200

    Add exception kind for docParamListType

---
+++ src/templates/docParamListType/exception.ts
@@ -0,0 +1,24 @@
+import { Element } from '@rgrove/parse-xml';
+import { Mappers, applyToChildrenGrouped } from '..';
+import Handlebars from 'handlebars';
+
+import docParamListItem from '../docParamListItem';
+
+const template = Handlebars.compile(
+  `
+**Exceptions**:
+{{#each parameteritem}}
+* {{this}}
+{{/each}}
+`,
+  { noEscape: true }
+);
+
+const mappers: Mappers = {
+  parameteritem: docParamListItem,
+};
+
+export default (element: Element) => {
+  const context = applyToChildrenGrouped(mappers)(element);
+  return template(context);
+};
