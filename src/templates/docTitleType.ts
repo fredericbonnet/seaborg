f6df0ac0fa60fdc0e83e53b4a8d1bbb69cb39e2f
Frédéric Bonnet <fredericbonnet@free.fr>
2019-09-28 15:18:57 +0200

    Fix missing text content in docTitleType

--- src/templates/docTitleType.ts
+++ src/templates/docTitleType.ts
@@ -5,12 +5,14 @@
 */
 
 import { Element } from '@rgrove/parse-xml';
-import { Mappers, applyToChildren, $default } from '.';
+import { Mappers, applyToChildren, $default, $text } from '.';
 
 import docTitleCmdGroup from './docTitleCmdGroup';
+import textNode from './textNode';
 
 const mappers: Mappers = {
   [$default]: docTitleCmdGroup,
+  [$text]: textNode,
 };
 
 export default (element: Element) =>
