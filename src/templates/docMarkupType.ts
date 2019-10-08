cfd7111ec1b9e5a1770c11189017da22df3842cc
Frédéric Bonnet <fredericbonnet@free.fr>
2019-10-08 23:16:35 +0200

    add docMarkupType

--- src/templates/docMarkupType.ts
+++ src/templates/docMarkupType.ts
@@ -1,4 +1,19 @@
-//TODO
+/*
+  <xsd:complexType name="docMarkupType" mixed="true">
+    <xsd:group ref="docCmdGroup" minOccurs="0" maxOccurs="unbounded" />
+  </xsd:complexType>
+*/
+
 import { Element } from '@rgrove/parse-xml';
+import { Mappers, $default, $text, applyToChildren } from '.';
+
+import docCmdGroup from './docCmdGroup';
+import textNode from './textNode';
+
+const mappers = (): Mappers => ({
+  [$default]: docCmdGroup,
+  [$text]: textNode,
+});
+
 export default (element: Element) =>
-  element.name + ' ' + JSON.stringify(element);
+  applyToChildren(mappers())(element).join('');
