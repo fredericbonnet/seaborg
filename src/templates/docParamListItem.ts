259a3d1e959b6af88beb89f20bde1db684c67d1c
Frédéric Bonnet <fredericbonnet@free.fr>
2019-10-11 22:14:56 +0200

    Add docParamListItem and subtypes

--- src/templates/docParamListItem.ts
+++ src/templates/docParamListItem.ts
@@ -1,3 +1,31 @@
+/*
+  <xsd:complexType name="docParamListItem">
+    <xsd:sequence>
+      <xsd:element name="parameternamelist" type="docParamNameList" minOccurs="0" maxOccurs="unbounded" />
+      <xsd:element name="parameterdescription" type="descriptionType" />
+    </xsd:sequence>
+  </xsd:complexType>
+*/
+
 import { Element } from '@rgrove/parse-xml';
-export default (element: Element) =>
-  element.name + ' ' + JSON.stringify(element);
+import { Mappers, applyToChildrenGrouped } from '.';
+import Handlebars from 'handlebars';
+
+import docParamNameList from './docParamNameList';
+import descriptionType from './descriptionType';
+
+const template = Handlebars.compile(
+  `**{{parameternamelist}}**{{#if parameterdescription}}: {{parameterdescription}}{{/if}}`,
+  { noEscape: true }
+);
+
+const mappers = (): Mappers => ({
+  parameternamelist: docParamNameList,
+  parameterdescription: descriptionType,
+});
+
+export default (element: Element) => {
+  const context = applyToChildrenGrouped(mappers())(element);
+
+  return template(context);
+};
