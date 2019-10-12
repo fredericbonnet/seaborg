c115db8630276010aaa0b69bfe4362dac3e773c5
Frédéric Bonnet <fredericbonnet@free.fr>
2019-10-12 23:07:12 +0200

    Add function pipe, not operator, and withAttribute filter

--- src/operators/index.ts
+++ src/operators/index.ts
@@ -1,6 +1,20 @@
 import { NodeBase, Element, Text } from '@rgrove/parse-xml';
 
-// Operators
+// Generic operators
+
+/**
+ * Function pipe
+ *
+ * @see https://medium.com/javascript-scene/reduce-composing-software-fe22f0c39a1d
+ */
+// @ts-ignore
+export const pipe = (...fns) => x => fns.reduce((v, f) => f(v), x);
+
+/** Negation operator */
+// @ts-ignore
+export const not = v => !v;
+
+// XML operators
 
 /**
  * Filter XML nodes with given type
@@ -16,6 +30,15 @@ export const withType = (type: string) => (node: NodeBase) =>
  * @param name Element name to select
  */
 export const withName = (name: string) => (node: Element) => node.name === name;
+
+/**
+ * Filter XML element nodes with given attribute value
+ *
+ * @param name Attribute name to check
+ * @param value Attribute value to select
+ */
+export const withAttribute = (name: string, value: string) => (node: Element) =>
+  node.attributes[name] === value;
 
 /** Map XML node to element node */
 export const asElementNode = (node: NodeBase) => node as Element;
