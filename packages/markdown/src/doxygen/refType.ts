fe2b03d47313f6850e0bcd71e02d5678ab8cd272
Frédéric Bonnet <fredericbonnet@free.fr>
2019-10-25 18:31:24 +0200

    Add refType

---
+++ packages/markdown/src/doxygen/refType.ts
@@ -0,0 +1,33 @@
+/*
+  <xsd:complexType name="refType">
+    <xsd:simpleContent>
+      <xsd:extension base="xsd:string">
+        <xsd:attribute name="refid" type="xsd:string" />
+        <xsd:attribute name="prot" type="DoxProtectionKind" use="optional"/>
+      </xsd:extension>
+    </xsd:simpleContent>
+  </xsd:complexType>
+*/
+
+import { Element } from '@rgrove/parse-xml';
+import Handlebars from 'handlebars';
+
+import { Mappers, applyToChildren, $text } from '../mappers';
+import { textNode } from '../generic';
+
+const template = Handlebars.compile('{{ref refid "compound" text}}', {
+  noEscape: true,
+});
+
+const mappers = (): Mappers => ({
+  [$text]: textNode,
+});
+
+export default (element: Element) => {
+  // TODO other attributes?
+  const {
+    attributes: { refid },
+  } = element;
+  const text = applyToChildren(mappers())(element).join('');
+  return template({ refid, text });
+};
