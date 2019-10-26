78a4b005cecb275aa4410dd81ed6fa362509a95b
Frédéric Bonnet <fredericbonnet@free.fr>
2019-10-26 22:52:34 +0200

    Add event kind for memberdefType

---
+++ packages/markdown/src/doxygen/memberdefType/event.ts
@@ -0,0 +1,47 @@
+import { Element } from '@rgrove/parse-xml';
+import Handlebars from 'handlebars';
+
+import { Mappers, applyToChildrenGrouped, $default } from '../../mappers';
+import { xsdString } from '../../generic';
+import { linkedTextType } from '..';
+
+import { mappers as defaultMappers } from '.';
+
+const template = Handlebars.compile(
+  `
+<a id="{{id}}"></a>
+### Event {{md name}}
+
+{{location}}
+
+\`\`\`c
+{{definition}}{{argsstring}}
+\`\`\`
+
+{{> memberdef-description}}
+
+{{#if type}}**Type**: {{type}}{{/if}}
+
+{{> memberdef-references}}
+
+{{TODO TODO}}
+`,
+  { noEscape: true }
+);
+
+const mappers = (): Mappers => ({
+  ...defaultMappers(),
+  type: linkedTextType,
+  definition: xsdString,
+  argsstring: xsdString,
+});
+
+// TODO attributes (e.g. static)
+export default (element: Element) => {
+  const {
+    attributes: { id },
+  } = element;
+  const context = applyToChildrenGrouped(mappers())(element);
+
+  return template({ ...context, id, TODO: context[$default] });
+};
