06dc8726296e2a4401cc2d1de7861992426eef5f
Frédéric Bonnet <fredericbonnet@free.fr>
2019-10-26 19:02:11 +0200

    Add namespace kind for compounddefType

---
+++ packages/markdown/src/doxygen/compounddefType/namespace.ts
@@ -0,0 +1,44 @@
+import { Element } from '@rgrove/parse-xml';
+import Handlebars from 'handlebars';
+
+import { Mappers, applyToChildrenGrouped, $default } from '../../mappers';
+import { ignore } from '../../operators';
+import { locationType } from '..';
+
+import { mappers as defaultMappers } from '.';
+
+const template = Handlebars.compile(
+  `
+# Namespace {{md compoundname}}
+
+{{location}}
+
+{{> compounddef-description}}
+
+{{> compounddef-list list=innernamespace label="Child namespaces"}}
+
+{{> compounddef-list list=innerclass label="Classes"}}
+
+{{> compounddef-sections}}
+
+{{> compounddef-source}}
+
+{{TODO TODO}}
+`,
+  { noEscape: true }
+);
+
+const mappers = (): Mappers => ({
+  ...defaultMappers(),
+  location: locationType,
+});
+
+export default (element: Element) => {
+  // TODO map kind to string
+  const {
+    attributes: { kind },
+  } = element;
+  const context = applyToChildrenGrouped(mappers())(element);
+
+  return template({ ...context, kind, TODO: context[$default] });
+};
