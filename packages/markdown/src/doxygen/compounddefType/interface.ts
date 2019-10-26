9c9ffeb78f138de2c9685c427808b18112e77fb3
Frédéric Bonnet <fredericbonnet@free.fr>
2019-10-26 23:44:30 +0200

    Add interface kind for compounddefType

---
+++ packages/markdown/src/doxygen/compounddefType/interface.ts
@@ -0,0 +1,60 @@
+import { Element } from '@rgrove/parse-xml';
+import Handlebars from 'handlebars';
+
+import { Mappers, applyToChildrenGrouped, $default } from '../../mappers';
+import { locationType, listofallmembersType, compoundRefType } from '..';
+
+import { mappers as defaultMappers } from '.';
+
+const template = Handlebars.compile(
+  `
+# Interface {{md compoundname}}
+
+{{location}}
+
+{{> compounddef-description}}
+
+{{#if basecompoundref}}
+**Inherits from**:
+
+{{#each basecompoundref}}
+* {{this}}
+{{/each}}
+{{/if}}
+
+{{#if derivedcompoundref}}
+**Inherited by**:
+
+{{#each derivedcompoundref}}
+* {{this}}
+{{/each}}
+{{/if}}
+
+{{listofallmembers}}
+
+{{> compounddef-sections}}
+
+{{> compounddef-source}}
+
+{{TODO TODO}}
+`,
+  { noEscape: true }
+);
+
+const mappers = (): Mappers => ({
+  ...defaultMappers(),
+  location: locationType,
+  basecompoundref: compoundRefType,
+  derivedcompoundref: compoundRefType,
+  listofallmembers: listofallmembersType,
+});
+
+export default (element: Element) => {
+  // TODO map kind to string
+  const {
+    attributes: { kind },
+  } = element;
+  const context = applyToChildrenGrouped(mappers())(element);
+
+  return template({ ...context, kind, TODO: context[$default] });
+};
