379f6a55e119ce2b94263626be68d7ce54fbde56
Frédéric Bonnet <fredericbonnet@free.fr>
2019-10-28 15:53:58 +0100

    Add badges for protection and bool attributes

--- packages/markdown/src/helpers/badges.ts
+++ packages/markdown/src/helpers/badges.ts
@@ -6,6 +6,7 @@
 
 import Handlebars from 'handlebars';
 import { context } from '@seaborg/core';
+import { DoxLanguage, DoxBool, DoxProtectionKind } from '../doxygen';
 
 /** Handlebars helper for Shields.io badges */
 const badgeHelper = (
@@ -20,10 +21,30 @@ const badgeHelper = (
 };
 
 /** Handlebars helper for language badges */
-const languageBadgeHelper = (code: string) =>
+const languageBadgeHelper = (code: DoxLanguage) =>
   badgeHelper(code, 'language', code, 'blue');
+
+/** Handlebars helper for protection badges */
+const protectionBadgeHelper = (code: DoxProtectionKind) => {
+  switch (code) {
+    case 'public':
+      return badgeHelper('public', '', 'public', 'brightgreen');
+    case 'protected':
+      return badgeHelper('protected', '', 'protected', 'yellow');
+    case 'private':
+      return badgeHelper('private', '', 'private', 'red');
+    case 'package':
+      return badgeHelper('package', '', 'package', 'blueviolet');
+  }
+};
+
+/** Handlebars helper for boolean badges */
+const boolBadgeHelper = (label: string, color: string, value: DoxBool) =>
+  value === 'yes' ? badgeHelper(label, '', label, color) : '';
 
 export function registerHelpers() {
   Handlebars.registerHelper('badge', badgeHelper);
   Handlebars.registerHelper('language-badge', languageBadgeHelper);
+  Handlebars.registerHelper('protection-badge', protectionBadgeHelper);
+  Handlebars.registerHelper('bool-badge', boolBadgeHelper);
 }
