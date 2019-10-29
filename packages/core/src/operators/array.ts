927a333295bb6356773bf63df1d78530d28f59c5
Frédéric Bonnet <fredericbonnet@free.fr>
2019-10-29 18:55:23 +0100

    Add array sort pipe operator

--- packages/core/src/operators/array.ts
+++ packages/core/src/operators/array.ts
@@ -16,9 +16,15 @@ export const map = <T, U>(f: MapFunc<T, U>) => (a: Array<T>) => a.map(f);
 export const flatMap = <T, U>(f: MapFunc<T, U>) => (a: Array<T>) =>
   a.flatMap(f);
 
-/** Array reduce pipe function */
+/** Array reduce function */
 export type ReduceFunc<T, U> = (u: U, t: T) => U;
 
 /** Array reduce pipe operator */
 export const reduce = <T, U>(f: ReduceFunc<T, U>, u: U) => (a: Array<T>) =>
   a.reduce(f, u);
+
+/** Array compare function */
+export type CompareFunc<T> = (a: T, b: T) => number;
+
+/** Array sort pipe operator */
+export const sort = <T>(f?: CompareFunc<T>) => (a: Array<T>) => a.sort(f);
