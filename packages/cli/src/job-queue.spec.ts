79d4894f795d172a66478cedeac1b33d864d6097
Frédéric Bonnet <fredericbonnet@free.fr>
2019-12-16 15:08:00 +0100

    Add pooled job queue adapter

--- packages/cli/src/job-queue.spec.ts
+++ packages/cli/src/job-queue.spec.ts
@@ -1,7 +1,13 @@
 import 'mocha';
 import { expect } from 'chai';
 
-import { Job, JobQueue, JobQueueFactory } from './job-queue';
+import {
+  Job,
+  JobQueue,
+  JobQueueFactory,
+  SimpleJobQueueAdapter,
+  PooledJobQueueAdapter,
+} from './job-queue';
 
 describe('JobQueue', () => {
   let jobQueue: JobQueue;
@@ -22,7 +28,7 @@ describe('JobQueue', () => {
     expect(result).to.eql('done');
   });
 
-  it('runs several jobs in parallel', async () => {
+  it('runs several jobs asynchronously', async () => {
     const jobs = [
       jobQueue.enqueue(async () => 1),
       jobQueue.enqueue(async () => 2),
@@ -30,5 +36,94 @@ describe('JobQueue', () => {
     ];
     const results = await Promise.all(jobs);
     expect(results).to.eql([1, 2, 3]);
+  });
+});
+
+describe('SimpleJobQueueAdapter', () => {
+  let jobQueue: JobQueue;
+  beforeEach(() => {
+    jobQueue = new SimpleJobQueueAdapter();
+  });
+
+  it('runs all jobs in parallel', async () => {
+    let sequence: any[] = [];
+    const jobs = [
+      jobQueue.enqueue(
+        () =>
+          new Promise((resolve, reject) => {
+            sequence.push('a');
+            setTimeout(() => {
+              sequence.push('b');
+              resolve(1);
+            }, 0);
+          })
+      ),
+      jobQueue.enqueue(async () => {
+        sequence.push('c');
+        return 2;
+      }),
+    ];
+    const results = await Promise.all(jobs);
+    expect(results).to.eql([1, 2]);
+    expect(sequence).to.eql(['a', 'c', 'b']);
+  });
+});
+
+describe('PooledJobQueueAdapter', () => {
+  specify('with pool capacity = 1', async () => {
+    const jobQueue = new PooledJobQueueAdapter(1);
+    let sequence: any[] = [];
+    const jobs = [
+      jobQueue.enqueue(
+        () =>
+          new Promise((resolve, reject) => {
+            sequence.push('a');
+            setTimeout(() => {
+              sequence.push('b');
+              resolve(1);
+            }, 0);
+          })
+      ),
+      jobQueue.enqueue(async () => {
+        sequence.push('c');
+        return 2;
+      }),
+    ];
+    const results = await Promise.all(jobs);
+    expect(results).to.eql([1, 2]);
+    expect(sequence).to.eql(['a', 'b', 'c']);
+  });
+  specify('with pool capacity = 2', async () => {
+    const jobQueue = new PooledJobQueueAdapter(2);
+    let sequence: any[] = [];
+    const jobs = [
+      jobQueue.enqueue(
+        () =>
+          new Promise((resolve, reject) => {
+            sequence.push('a');
+            setTimeout(() => {
+              sequence.push('b');
+              resolve(1);
+            }, 0);
+          })
+      ),
+      jobQueue.enqueue(
+        () =>
+          new Promise((resolve, reject) => {
+            sequence.push('c');
+            setTimeout(() => {
+              sequence.push('d');
+              resolve(2);
+            }, 0);
+          })
+      ),
+      jobQueue.enqueue(async () => {
+        sequence.push('e');
+        return 3;
+      }),
+    ];
+    const results = await Promise.all(jobs);
+    expect(results).to.eql([1, 2, 3]);
+    expect(sequence).to.eql(['a', 'c', 'b', 'd', 'e']);
   });
 });
--- packages/cli/src/job-queue.ts
+++ packages/cli/src/job-queue.ts
@@ -5,23 +5,86 @@ export type Job = () => Promise<any>;
  * Job queue interface
  * */
 export interface JobQueue {
+  /**
+   * Enqueue a job
+   *
+   * @param job Job function to enqueue
+   */
   enqueue(job: Job): Promise<any>;
 }
 
 /**
  * Simple job queue implementation, simply run the jobs immediately
  */
-class SimpleJobQueueAdapter implements JobQueue {
+export class SimpleJobQueueAdapter implements JobQueue {
+  constructor() {}
+
   enqueue(job: Job) {
     return new Promise((resolve, reject) => {
       setTimeout(() => {
-        try {
-          resolve(job());
-        } catch (e) {
-          reject(e);
-        }
+        job()
+          .then(resolve)
+          .catch(reject);
       }, 0);
     });
+  }
+}
+
+/**
+ * Pooled job queue implementation, limit the number of parallel jobs
+ */
+export class PooledJobQueueAdapter implements JobQueue {
+  /** Enqueued jobs */
+  private jobs: { job: Job; resolve: Function; reject: Function }[] = [];
+
+  /** Running jobs */
+  private runningJobs: Job[] = [];
+
+  /** Number of running jobs  */
+  private nbRunningJobs: number = 0;
+
+  /**
+   * Constructor
+   *
+   * @param capacity Pool capacity
+   */
+  constructor(public capacity: number) {
+    this.runningJobs.length = capacity;
+  }
+
+  enqueue(job: Job): Promise<any> {
+    return new Promise((resolve, reject) => {
+      this.jobs.push({ job, resolve, reject });
+      this.processQueue();
+    });
+  }
+
+  /** Run all jobs up to the pool capacity */
+  private processQueue() {
+    while (this.jobs.length && this.nbRunningJobs < this.capacity) {
+      this.runNextJob();
+    }
+  }
+
+  /** Run next job in first free pool */
+  private runNextJob() {
+    if (!this.jobs.length) return;
+    // @ts-ignore
+    const { job, resolve, reject } = this.jobs.shift();
+
+    const freePool = this.runningJobs.findIndex(e => !e);
+    this.runningJobs[freePool] = job;
+    this.nbRunningJobs++;
+    setTimeout(() => {
+      job()
+        .then(resolve)
+        .catch(reject)
+        .finally(() => {
+          delete this.runningJobs[freePool];
+          this.nbRunningJobs--;
+          this.processQueue();
+        });
+    }, 0);
   }
 }
 
@@ -29,6 +92,9 @@ class SimpleJobQueueAdapter implements JobQueue {
 export class JobQueueFactory {
   static create(): JobQueue {
     return new SimpleJobQueueAdapter();
+  }
+  static createPooled(capacity: number): JobQueue {
+    return new PooledJobQueueAdapter(capacity);
   }
 }
 
