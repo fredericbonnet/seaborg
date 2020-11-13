d0893028ef54a65573b1037412557b7d19731217
Frédéric Bonnet <fredericbonnet@free.fr>
2020-11-13 01:02:09 +0100

    Fix version in CLI

--- packages/cli/src/args.ts
+++ packages/cli/src/args.ts
@@ -18,7 +18,7 @@ export const parseArgs = () => {
   program
     .name('seaborg')
     .description('Seaborg converts Doxygen XML output to Markdown')
-    .version('0.3.0')
+    .version('0.4.1')
     .option(
       '-c, --config-file <file>',
       'configuration file',
